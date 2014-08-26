a2bba6648ba5808b96ee1dea3424f48bc1ef54ca
rakeshda7 <305031526@HCA-H8W38R1.clients.ap.health.ge.com>
2014-08-27 00:22:24 +0530

    Implemented My Own Heap.

---
+++ Heap/includes/Heap.h
@@ -0,0 +1,25 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+#include <vector>
+
+using namespace std;
+
+class Heap {
+    vector<int> v;
+    bool minHeap;
+
+    int getParentNodeIndex(int);
+    void push_up(unsigned int index);
+    void push_down(unsigned int index);
+    bool isLesser(int x, int y);
+
+public:
+    Heap(bool isMinHeap=true): minHeap(isMinHeap) {}
+    void push(int i);
+    int pop();
+    int getLen();
+    void print();
+};
+
+#endif // HEAP_H
