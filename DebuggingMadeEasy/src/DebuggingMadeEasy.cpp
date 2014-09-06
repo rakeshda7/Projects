9d83b4e388bbdfa16e83d266e1a9a9a0ad6c24a5
rakeshda7 <305031526@HCA-H8W38R1.clients.ap.health.ge.com>
2014-09-07 02:09:37 +0530

    Some more backing, but still nothing works!!!

--- DebuggingMadeEasy/src/DebuggingMadeEasy.cpp
+++ DebuggingMadeEasy/src/DebuggingMadeEasy.cpp
@@ -6,6 +6,10 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 #include <vector>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -14,8 +18,8 @@ using namespace std;
 class LogFileIf
 {
 public:
-	string getLine()=0;
-	string getTime()=0;
+	virtual string getLine()=0;
+	virtual string getTime()=0;
 };
 //*********End LogFileIf***************
 
@@ -23,9 +27,29 @@ public:
 //Interface for log file
 class AbstractLogFile: public LogFileIf
 {
+protected:
+	string line;
+	ifstream ifs;
+
 public:
-	string getLine() override {return nullptr;}
-	string getTime() override {return nullptr;}
+	AbstractLogFile(string name) {
+		cout<<"AbstractLogFile."<<endl;
+		ifs.open(name.c_str(), std::ifstream::in);
+		getline(ifs, line);
+	}
+
+	string getLine() override {
+		cout<<"AbstractLogFile:getLine."<<endl;
+		if(ifs.eof()) {
+			throw "noMoreLogLines";
+		}
+		string tmpLine = line;
+		getline(ifs, line);
+		return tmpLine;
+	}
+	string getTime() override {
+		return nullptr;
+	}
 };
 //*********End AbstractLogFile***************
 
@@ -34,8 +58,7 @@ public:
 class scnLogFile: public AbstractLogFile
 {
 public:
-	string getLine() override {return nullptr;}
-	string getTime() override {return nullptr;}
+	scnLogFile(string name): AbstractLogFile(name) {}
 };
 //*********End scnLogFile***************
 
@@ -44,8 +67,7 @@ public:
 class wfmLogFile: public AbstractLogFile
 {
 public:
-	string getLine() override {return nullptr;}
-	string getTime() override {return nullptr;}
+	wfmLogFile(string name): AbstractLogFile(name) {}
 };
 //*********End wfmLogFile***************
 
@@ -54,8 +76,28 @@ public:
 class gslLogFile: public AbstractLogFile
 {
 public:
-	string getLine() override {return nullptr;}
-	string getTime() override {return nullptr;}
+	gslLogFile(string name): AbstractLogFile(name) {}
+};
+//*********End gslLogFile***************
+
+//*************gslLogFile***************
+//Interface for log file
+class LogFileFactory
+{
+public:
+	static LogFileIf* getInstance(const string &name, const string &type) {
+		LogFileIf *lgf = nullptr;
+		if(type=="-scn") {
+			lgf = new scnLogFile(name);
+		}
+		else if(type=="-wfm") {
+			lgf = new wfmLogFile(name);
+		}
+		else if(type=="-gsl") {
+			lgf = new gslLogFile(name);
+		}
+		return lgf;
+	}
 };
 //*********End gslLogFile***************
 
@@ -63,16 +105,56 @@ public:
 //Merge contents based on time stamp
 class mergeFiles
 {
-	vector<LogFileIf> logFiles;
+	vector<LogFileIf*> logFiles;
+
+	string getLineWithMinTime() {
+		string minTime = logFiles.at(0)->getTime();
+		string minLine = logFiles.at(0)->getLine();
+
+		for(LogFileIf *lgf: logFiles) {
+			if(minTime>lgf->getTime()) {
+				minTime=lgf->getTime();
+				minLine=lgf->getLine();
+			}
+		}
+		return minLine;
+	}
 
 public:
-	string getNextLine() {return nullptr;};
+	string getNextLine() {
+		if(logFiles.size()==0) {
+			cout<<"throwing noLogFiles"<<endl;
+			throw "noLogFiles";
+		}
+		else if(logFiles.size()==1) {
+			cout<<"else if"<<endl;
+			return logFiles.at(0)->getLine();
+		}
+		else {
+			cout<<"else"<<endl;
+			return getLineWithMinTime();
+		}
+	}
+	void addFile(string name, string type) {
+		logFiles.push_back(LogFileFactory::getInstance(name, type));
+	}
 };
 //*********End MergeFiles**************
 
+void parseCommandLine(int argc, char* argv[], mergeFiles &mf)
+{
+	for(int i=0; i<argc; i++) {
+		if(strcmp(argv[i],"-scn")==0 ||
+			strcmp(argv[i],"-wfm")==0 ||
+			strcmp(argv[i],"-gsl")==0) {
+			mf.addFile(argv[i], argv[i+1]); i++;
+		}
+	}
+}
+
 int main(int argc, char* argv[]) {
 	//Usage:
-	//	./a.out [-<fileType> <file>] ... -<action>
+	//	./a.out [-<fileType> <file>]... [-<action>]...
 	//		fileType:
 	//			-scn
 	//			-wfm
@@ -81,5 +163,16 @@ int main(int argc, char* argv[]) {
 	//			-print
 	//			-grep
 	//			-stime -etime
+
+	mergeFiles mf;
+
+	//Parse Command Line Arguments
+	parseCommandLine(argc, argv, mf);
+
+	for(string line=mf.getNextLine(); 1 ; line=mf.getNextLine()) {
+		cout<<"InFor"<<endl;
+		cout<<line;
+	}
+
 	return 0;
 }
