d43bd3c2012fce888e3f7165eb6d3285f8fd31af
rakeshda7 <305031526@HCA-H8W38R1.clients.ap.health.ge.com>
2014-09-10 16:06:24 +0530

    Final for WFM

--- DebuggingMadeEasy/src/DebuggingMadeEasy.cpp
+++ DebuggingMadeEasy/src/DebuggingMadeEasy.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : DebuggingMadeEasy.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -45,10 +45,10 @@ static inline std::string &trim(std::string &s) {
 class LogFileIf
 {
 public:
-	virtual string getLine()=0;
-	virtual string getTime()=0;
-	virtual string getName()=0;
-	virtual size_t getLineSize()=0;
+        virtual string getLine()=0;
+        virtual string getTime()=0;
+        virtual string getName()=0;
+        virtual size_t getLineSize()=0;
 };
 //*********End LogFileIf***************
 
@@ -57,124 +57,125 @@ public:
 class AbstractLogFile: public LogFileIf
 {
 protected:
-	ifstream ifs;
-	string name;
+        ifstream ifs;
+        string name;
 
-	string line;
-	string nextLine;
-	bool eofReceived=false;
+        string line;
+        string nextLine;
+        bool eofReceived;
 
-	bool isValidTime(string time) {
-		size_t index = -1;
+        bool isValidTime(string time) {
+                size_t index = -1;
 
-		//The pattern "*:*:*.*" will match
-		if((index=time.find(':', 0))!=string::npos &&
-		   (index=time.find(':', index))!=string::npos &&
-		   (index=time.find('.', index))!=string::npos) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+                //The pattern "*:*:*.*" will match
+                if((index=time.find(':', 0))!=string::npos &&
+                   (index=time.find(':', index))!=string::npos &&
+                   (index=time.find('.', index))!=string::npos) {
+                        return true;
+                } else {
+                        return false;
+                }
+        }
 
-	//Check if Second Word is valid time and if valid return
-	string getTimeFromString(string line) {
-		istringstream is(line.c_str());
+        //Check if Second Word is valid time and if valid return
+        string getTimeFromString(string line) {
+                istringstream is(line.c_str());
 
-		string tmp;
-		// Get Second Word
-		is >> tmp >> tmp;
+                string tmp;
+                // Get Second Word
+                is >> tmp >> tmp;
 
-		if(isValidTime(tmp)) {
-			return tmp;
-		}
-		else {
-			string tmp;
-			return tmp;
-		}
-	}
+                if(isValidTime(tmp)) {
+                        return tmp;
+                }
+                else {
+                        string tmp;
+                        return tmp;
+                }
+        }
 
-	string getSingleLine() {
-		string line;
+        string getSingleLine() {
+                string line;
 
-		if(nextLine.size()>0) {
-			line=nextLine;
-			nextLine.clear();
-		}
-		else {
-			try {
-				getline(ifs, line);
-			} catch (ios_base::failure &e) {
-						eofReceived=true;
-			}
-		}
-		return line;
-	}
+                if(nextLine.size()>0) {
+                        line=nextLine;
+                        nextLine.clear();
+                }
+                else {
+                        try {
+                                getline(ifs, line);
+                        } catch (ios_base::failure &e) {
+                                                eofReceived=true;
+                        }
+                }
+                return line;
+        }
 
-	//Add any line without time to the previous line with time
-	string getCompleLine() {
-		string resLine;
+        //Add any line without time to the previous line with time
+        string getCompleLine() {
+                string resLine;
 
-		string tmpLine;
-		bool tmpLineHaveTime=false;
-		while(true) {
-			tmpLine=getSingleLine();
+                string tmpLine;
+                bool tmpLineHaveTime=false;
+                while(true) {
+                        tmpLine=getSingleLine();
 
-			//Trim any white space
-			tmpLine=trim(tmpLine);
+                        //Trim any white space
+                        tmpLine=trim(tmpLine);
 
-			//No line exists
-			if(tmpLine.size()==0) {
-				if(eofReceived) {
-					break;
-				}
-				////Empty line received, Ignore
-			}
-			//Have atleast one line with time
-			else if(getTimeFromString(tmpLine).size()>0 && !tmpLineHaveTime) {
-				tmpLineHaveTime=true;
-				resLine = tmpLine;
-			}
-			//Second line with time received
-			else if(getTimeFromString(tmpLine).size()>0){
-				nextLine=tmpLine;
-				break;
-			}
-			//Line without time received
-			else {
-				resLine = resLine +"\n"+ tmpLine;
-			}
-		}
-		return resLine;
-	}
+                        //No line exists
+                        if(tmpLine.size()==0) {
+                                if(eofReceived) {
+                                        break;
+                                }
+                                ////Empty line received, Ignore
+                        }
+                        //Have atleast one line with time
+                        else if(getTimeFromString(tmpLine).size()>0 && !tmpLineHaveTime) {
+                                tmpLineHaveTime=true;
+                                resLine = tmpLine;
+                        }
+                        //Second line with time received
+                        else if(getTimeFromString(tmpLine).size()>0){
+                                nextLine=tmpLine;
+                                break;
+                        }
+                        //Line without time received
+                        else {
+                                resLine = resLine +"\n"+ tmpLine;
+                        }
+                }
+                return resLine;
+        }
 
 public:
-	AbstractLogFile(string name) {
-		this->name=name;
-		ifs.open(name.c_str(), std::ifstream::in);
-		ifs.exceptions(ifstream::eofbit);
-		line=getCompleLine();
-	}
+        AbstractLogFile(string name) {
+                eofReceived=false;
+                this->name=name;
+                ifs.open(name.c_str(), std::ifstream::in);
+                ifs.exceptions(ifstream::eofbit);
+                line=getCompleLine();
+        }
 
-	string getLine() override {
-		string tmpLine = getCompleLine();
+        string getLine() {
+                string tmpLine = getCompleLine();
 
-		swap(line, tmpLine);
+                swap(line, tmpLine);
 
-		return tmpLine;
-	}
+                return tmpLine;
+        }
 
-	string getTime() override {
-		return getTimeFromString(line);
-	}
+        string getTime() {
+                return getTimeFromString(line);
+        }
 
-	size_t getLineSize() override {
-		return line.size();
-	}
+        size_t getLineSize() {
+                return line.size();
+        }
 
-	string getName() override {
-		return name;
-	}
+        string getName() {
+                return name;
+        }
 };
 //*********End AbstractLogFile***************
 
@@ -183,7 +184,7 @@ public:
 class scnLogFile: public AbstractLogFile
 {
 public:
-	scnLogFile(string name): AbstractLogFile(name) {}
+        scnLogFile(string name): AbstractLogFile(name) {}
 };
 //*********End scnLogFile***************
 
@@ -192,7 +193,7 @@ public:
 class wfmLogFile: public AbstractLogFile
 {
 public:
-	wfmLogFile(string name): AbstractLogFile(name) {}
+        wfmLogFile(string name): AbstractLogFile(name) {}
 };
 //*********End wfmLogFile***************
 
@@ -201,7 +202,7 @@ public:
 class gslLogFile: public AbstractLogFile
 {
 public:
-	gslLogFile(string name): AbstractLogFile(name) {}
+        gslLogFile(string name): AbstractLogFile(name) {}
 };
 //*********End gslLogFile***************
 
@@ -210,19 +211,19 @@ public:
 class LogFileFactory
 {
 public:
-	static LogFileIf* getInstance(const string &name, const string &type) {
-		LogFileIf *lgf = nullptr;
-		if(type.compare("-scn")==0) {
-			lgf = new scnLogFile(name);
-		}
-		else if(type.compare("-wfm")==0) {
-			lgf = new wfmLogFile(name);
-		}
-		else if(type.compare("-gsl")==0) {
-			lgf = new gslLogFile(name);
-		}
-		return lgf;
-	}
+        static LogFileIf* getInstance(const string &name, const string &type) {
+                LogFileIf *lgf = NULL;
+                if(type.compare("-scn")==0) {
+                        lgf = new scnLogFile(name);
+                }
+                else if(type.compare("-wfm")==0) {
+                        lgf = new wfmLogFile(name);
+                }
+                else if(type.compare("-gsl")==0) {
+                        lgf = new gslLogFile(name);
+                }
+                return lgf;
+        }
 };
 //*********End gslLogFile***************
 
@@ -230,79 +231,79 @@ public:
 //Merge contents based on time stamp
 class mergeFiles
 {
-	vector<LogFileIf*> logFiles;
+        vector<LogFileIf*> logFiles;
 
-	string getLineWithMinTime() {
+        string getLineWithMinTime() {
 
-		string result;
+                string result;
 
-		// When no log file
-		if(logFiles.size()==0) {
-			return result;
-		}
+                // When no log file
+                if(logFiles.size()==0) {
+                        return result;
+                }
 
-		//When one or more log file
-		LogFileIf *resLgf = nullptr;
-		for(LogFileIf *lgf: logFiles) {
-			if(resLgf==nullptr) {
-				if(lgf->getTime().size()>0) {
-						resLgf=lgf;
-				}
-				continue;
-			}
-			if(resLgf!=nullptr && resLgf->getTime()>lgf->getTime()) {
-				resLgf=lgf;
-			}
-		}
+                //When one or more log file
+                LogFileIf *resLgf = NULL;
+                for(unsigned int i=0; i<logFiles.size(); i++) {
+                        if(resLgf==NULL) {
+                                if(logFiles.at(i)->getTime().size()>0) {
+                                        resLgf=logFiles.at(i);
+                                }
+                                continue;
+                        }
+                        if(resLgf->getTime()>logFiles.at(i)->getTime()) {
+                                resLgf=logFiles.at(i);
+                        }
+                }
 
-		//Add the fileName to the line
-		if(resLgf->getLineSize()>0) {
-			result=resLgf->getName()+":"+resLgf->getLine();
-		}
-		return result;
-	}
+                //Add the fileName to the line
+                if(resLgf!=NULL && resLgf->getLineSize()>0) {
+                        result=resLgf->getName()+":"+resLgf->getLine();
+                }
+                return result;
+        }
 
 public:
-	string getNextLine() {
-		return getLineWithMinTime();
-	}
-	void addFile(string name, string type) {
-		logFiles.push_back(LogFileFactory::getInstance(name, type));
-	}
+        string getNextLine() {
+                return getLineWithMinTime();
+        }
+        void addFile(string name, string type) {
+                logFiles.push_back(LogFileFactory::getInstance(name, type));
+        }
 };
 //*********End MergeFiles**************
 
 void parseCommandLine(int argc, char* argv[], mergeFiles &mf)
 {
-	for(int i=0; i<argc; i++) {
-		if(strcmp(argv[i],"-scn")==0 ||
-			strcmp(argv[i],"-wfm")==0 ||
-			strcmp(argv[i],"-gsl")==0) {
-			mf.addFile(argv[i+1], argv[i]); i++;
-		}
-	}
+        for(int i=0; i<argc; i++) {
+                if(strcmp(argv[i],"-scn")==0 ||
+                        strcmp(argv[i],"-wfm")==0 ||
+                        strcmp(argv[i],"-gsl")==0) {
+                        mf.addFile(argv[i+1], argv[i]); i++;
+                }
+        }
 }
 
 int main(int argc, char* argv[]) {
-	//Usage:
-	//	./a.out [-<fileType> <file>]... [-<action>]...
-	//		fileType:
-	//			-scn
-	//			-wfm
-	//			-gsl
-	//		action:
-	//			-print
-	//			-grep
-	//			-stime -etime
+        //Usage:
+        //      ./a.out [-<fileType> <file>]... [-<action>]...
+        //              fileType:
+        //                      -scn
+        //                      -wfm
+        //                      -gsl
+        //              action:
+        //                      -print
+        //                      -grep
+        //                      -stime -etime
 
-	mergeFiles mf;
+        mergeFiles mf;
 
-	//Parse Command Line Arguments
-	parseCommandLine(argc, argv, mf);
+        //Parse Command Line Arguments
+        parseCommandLine(argc, argv, mf);
 
-	for(string line=mf.getNextLine(); line.size()>0; line=mf.getNextLine()) {
-		cout<<line<<endl;;
-	}
+        for(string line=mf.getNextLine(); line.size()>0; line=mf.getNextLine()) {
+                cout<<line<<endl;;
+        }
 
-	return 0;
+        return 0;
 }
